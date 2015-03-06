be98f127d5cdf2fc4edc46dbc5a8810d6f5b4215
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:11:36 +0800

    update

--- HolderAssets.php
+++ HolderAssets.php
@@ -7,30 +7,11 @@
 namespace xj\holder;
 
 use yii\web\AssetBundle;
-use yii;
 
 class HolderAssets extends AssetBundle {
 
+    public $sourcePath = '@vendor/xj/yii2-holder-widget/assets';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
-    public $js = [];
-    public $css = [];
-    public $depends = [
-//'yii\web\JqueryAsset',
-    ];
-
-    private function getJs() {
-        return [
-            YII_DEBUG ? 'holder.js' : 'holder.min.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
+    public $js = ['holder.js'];
 
 }
