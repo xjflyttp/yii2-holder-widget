a7d1e3c1c2f37c2a31aafaeb463545cadfbbe363
xjflyttp <xjflyttp@gmail.com>
2017-11-30 10:54:11 +0800

    fix basePath

--- HolderAssets.php
+++ HolderAssets.php
@@ -11,7 +11,6 @@ use yii\web\AssetBundle;
 class HolderAssets extends AssetBundle {
 
     public $sourcePath = '@vendor/xj/yii2-holder-widget/assets';
-    public $basePath = '@webroot/assets';
     public $js = ['holder.js'];
 
 }
